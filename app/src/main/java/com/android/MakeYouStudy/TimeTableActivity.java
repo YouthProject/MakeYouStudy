b18ed0a8c8a8a420aad219ab6b99f30f19c5a639
JJinTae <emflaxl100@naver.com>
2020-05-13 20:12:40 +0900

    Created EditActivity Layout - EditActivity Java file
    TimeTableActivity Java file Update

--- app/src/main/java/com/android/MakeYouStudy/TimeTableActivity.java
+++ app/src/main/java/com/android/MakeYouStudy/TimeTableActivity.java
@@ -1,26 +1,45 @@
 package com.android.MakeYouStudy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.tlaabs.timetableview.Schedule;
+import com.github.tlaabs.timetableview.TimetableView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class TimeTableActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class TimeTableActivity extends AppCompatActivity implements View.OnClickListener{
+    private Context context;
+    public static final int REQUEST_ADD = 1;
+    public static final int REQUEST_EDIT = 2;
 
     //firebase auth object
     private FirebaseAuth firebaseAuth;
 
     //view objects
     private TextView textViewUserEmail;
+    private Button addBtn;
+    private Button clearBtn;
+    private Button saveBtn;
+    private Button loadBtn;
+
+    private TimetableView timetable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +53,116 @@ public class TimeTableActivity extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         textViewUserEmail.setText("UserEmail : " + user.getEmail());
+
+        init();
+    }
+
+    private void init(){
+        int nWeek = doDayOfWeek();
+
+        this.context = this;
+        addBtn = findViewById(R.id.add_btn);
+        clearBtn = findViewById(R.id.clear_btn);
+        saveBtn = findViewById(R.id.save_btn);
+        loadBtn = findViewById(R.id.load_btn);
+
+        timetable = findViewById(R.id.timetable);
+        timetable.setHeaderHighlight(nWeek);
+        initView();
+    }
+
+    private void initView(){
+        addBtn.setOnClickListener(this);
+        clearBtn.setOnClickListener(this);
+        saveBtn.setOnClickListener(this);
+        loadBtn.setOnClickListener(this);
+
+        timetable.setOnStickerSelectEventListener(new TimetableView.OnStickerSelectedListener() {
+            @Override
+            public void OnStickerSelected(int idx, ArrayList<Schedule> schedules) {
+                Intent i = new Intent(context, EditActivity.class);
+                i.putExtra("mode",REQUEST_EDIT);
+                i.putExtra("idx", idx);
+                i.putExtra("schedules", schedules);
+                startActivityForResult(i,REQUEST_EDIT);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_btn:
+                Intent i = new Intent(this,EditActivity.class);
+                i.putExtra("mode",REQUEST_ADD);
+                startActivityForResult(i,REQUEST_ADD);
+                break;
+            case R.id.clear_btn:
+                timetable.removeAll();
+                break;
+            case R.id.save_btn:
+                saveByPreference(timetable.createSaveData());
+                break;
+            case R.id.load_btn:
+                loadSavedData();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ADD:
+                if (resultCode == EditActivity.RESULT_OK_ADD) {
+                    ArrayList<Schedule> item = (ArrayList<Schedule>) data.getSerializableExtra("schedules");
+                    timetable.add(item);
+                }
+                break;
+            case REQUEST_EDIT:
+                /** Edit -> Submit */
+                if (resultCode == EditActivity.RESULT_OK_EDIT) {
+                    int idx = data.getIntExtra("idx", -1);
+                    ArrayList<Schedule> item = (ArrayList<Schedule>) data.getSerializableExtra("schedules");
+                    timetable.edit(idx, item);
+                }
+                /** Edit -> Delete */
+                else if (resultCode == EditActivity.RESULT_OK_DELETE) {
+                    int idx = data.getIntExtra("idx", -1);
+                    timetable.remove(idx);
+                }
+                break;
+        }
+    }
+
+    /** save timetableView's data to SharedPreferences in json format */
+    private void saveByPreference(String data){
+        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = mPref.edit();
+        editor.putString("timetable_demo",data);
+        editor.commit();
+        Toast.makeText(this, data,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"saved!",Toast.LENGTH_SHORT).show();
+    }
+
+    /** get json data from SharedPreferences and then restore the timetable */
+    private void loadSavedData(){
+        timetable.removeAll();
+        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String savedData = mPref.getString("timetable_demo","");
+        if(savedData == null && savedData.equals("")) return;
+        timetable.load(savedData);
+        Toast.makeText(this,"loaded!",Toast.LENGTH_SHORT).show();
+    }
+
+    private int doDayOfWeek(){
+        Calendar cal = Calendar.getInstance();
+        int nWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if(nWeek==1){
+            nWeek = 7;
+        }else{
+            nWeek -= 1;
+        }
+        return nWeek;
     }
 }
