7d5d518d279cb8689a403e83bbb882a039a03ce8
KYUNGMINNA <nkm4095@naver.com>
2020-06-05 14:07:46 +0900

    Unnecessary code delete

--- app/src/main/java/com/android/MakeYouStudy/userinfo.java
+++ app/src/main/java/com/android/MakeYouStudy/userinfo.java
@@ -1,58 +1,25 @@
 package com.android.MakeYouStudy;
 public class userinfo {
     private String userName;
-
     private String profile;
-
     private String usercal;
-
     private String usertable;
-
     private String usercheck;
-
     private String userdiary;
-
-
     public userinfo(String userName, String profile) {
-
         this.userName = userName;
-
         this.profile = profile;
-
-        this.usercal=usercal;
-
-        this.usertable=usertable;
-
-        this.usercheck=usercheck;
-
-        this.userdiary=userdiary;
-
     }
-
-
     public String getUserName() {
-
         return userName;
-
     }
-
-
     public void setUserName(String userName) {
-
         this.userName = userName;
-
     }
-
-
     public String getProfile() {
-
         return profile;
-
     }
-
-
     public void setProfile(String profile) {
-
         this.profile = profile;
 
     }
